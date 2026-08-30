6e00a1322add04bb71cababd8ef40170d7397acd
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2026-08-30 06:20:42 +0530

    add new learning

---
+++ Syransh ai school/Code/loops.py
@@ -0,0 +1,6 @@
+
+
+a = range(1,21,1)
+
+for i in a:
+    print(i)
--- Syransh ai school/Code/main.py
+++ Syransh ai school/Code/main.py
@@ -2,4 +2,6 @@ print("hellow how are you gandu log kaise ho kya kar rhe ho ")
 
 a = " gandu ho app"
 
-print(a[0:10:8])
+print(a[0:10:8]);
+
+print(f"hello {a}")
