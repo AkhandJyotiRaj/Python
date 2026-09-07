ea33e9d636c16784dacc6a51a578e28cc4f2fd63
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2026-09-07 14:43:41 +0530

    learn py

---
+++ Syransh ai school/Code/function.py
@@ -0,0 +1,4 @@
+def bol():
+    print("lund loge kya tum mera bhai ho ya nahi?")
+    
+bol()
--- Syransh ai school/Code/loops.py
+++ Syransh ai school/Code/loops.py
@@ -1,6 +1,7 @@
-
-
-a = range(1,21,1)
-
-for i in a:
-    print(i)
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        if j % 2 == 0:
+            print(j, end=" ")
+        else:
+            print("*", end=" ")
+    print()
